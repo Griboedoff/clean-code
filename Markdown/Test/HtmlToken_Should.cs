917b8a6704b448059a0135f0480e75dba2553e4b
Griboedoff <igormb@mbp-igormb.at.urfu.ru>
2016-11-11 15:47:43 +0500

    add test for tags inside

--- Markdown/Test/HtmlToken_Should.cs
+++ Markdown/Test/HtmlToken_Should.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using FluentAssertions;
 namespace Markdown.Test
 {
@@ -11,6 +12,20 @@ namespace Markdown.Test
 			var token = new HtmlToken(Tag.Em, "data", 0);
 
 			token.ToString().Should().Be("<em>data</em>");
+		}
+
+		[Test]
+		public void ShouldConcatManyTags_WhenHasInsertedTags()
+		{
+			var tokenList = new List<HtmlToken>
+			{
+				new HtmlToken(Tag.Em, "italic", 0),
+				new HtmlToken(Tag.Empty, "empty", 0),
+				new HtmlToken(Tag.Strong, "bold", 0)
+			};
+
+			var token = new HtmlToken(Tag.Strong, tokenList, 0);
+			token.ToString().Should().Be("<strong><em>italic</em>empty<strong>bold</strong></strong>");
 		}
 	}
 }
